70d183cfd24b7dcb34a2b2490049002e557e8ef1
Oatmealcake <rlarhdms26@naver.com>
2021-07-14 16:35:15 +0900

    변화:createItem(), 추가: scroll, focus

--- shoppingApp/js/main.js
+++ shoppingApp/js/main.js
@@ -3,31 +3,46 @@
 const shopList = document.querySelector('.shop_list');
 const input = document.querySelector('.text_input');
 const addBtn = document.querySelector('.btn_add');
-delItem();
+
 addBtn.addEventListener('click', () => {
-  if (input.value === '') return;
   addItem();
-  delItem();
 })
 input.addEventListener('keyup', (ev) => {
-  if (ev.keyCode === 13) {
-    if (input.value === '') return;
+  if (ev.key === 'Enter') {
     addItem();
-    delItem();
   }
 })
 function addItem() {
-  let li = document.createElement('li');
-  li.innerHTML = `<span>${input.value}</span><button type="button" class="btn_del"><i class="fas fa-trash-alt"></i></button>`;
-  shopList.append(li);
-  input.value = '';
-}
-function delItem() {
-  let delBtn = document.querySelectorAll('.btn_del');
-  let lis = shopList.querySelectorAll('li');
-  for (let i = 0; i < lis.length; i++) {
-    delBtn[i].addEventListener('click', () => {
-      lis[i].remove();
-    })
+  const text = input.value;
+  if (input.value === '') {
+    input.focus(); // 포커스는 유지될 수 있도록
+    return;
   }
+  const item = createItem(text);
+  shopList.appendChild(item);
+  item.scrollIntoView({block: 'center'}) // 새로 추가된 아이템으로 스크롤링
+  input.value = '';
+  input.focus();
+}
+function createItem(text) {
+  const li = document.createElement('li');
+
+  const item = document.createElement('div');
+  item.setAttribute('class', 'item');
+
+  const name = document.createElement('span');
+  name.setAttribute('class', 'item_name');
+  name.innerText = text;
+
+  const delBtn = document.createElement('button');
+  delBtn.setAttribute('class', 'btn_del');
+  delBtn.innerHTML = '<i class="fas fa-trash-alt"></i>';
+  delBtn.addEventListener('click', () => {
+    shopList.removeChild(li);
+  })
+
+  item.appendChild(name);
+  item.appendChild(delBtn);
+  li.appendChild(item);
+  return li;
 }
