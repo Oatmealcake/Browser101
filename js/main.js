e87b11a85ca1cf13d3c0a760d3158f14979badeb
Oatmealcake <rlarhdms26@naver.com>
2021-07-20 11:29:30 +0900

    sound추가, timer수정

--- js/main.js
+++ js/main.js
@@ -7,40 +7,50 @@ const gameAmount = document.querySelector('.game_count');
 const gameArea = document.querySelector('.game_area');
 const btnPlay = document.querySelector('.game_control');
 const btnReplay = document.querySelector('.replay');
-let timer;
-let timerLeft;
+let started = false;
+let timer = undefined;
+let timerLeft = 5;
 let randomAmount = 0;
 
+const carrotSound = new Audio('./sound/carrot_pull.mp3');
+const bugSound = new Audio('./sound/bug_pull.mp3');
+const bgSound = new Audio('./sound/bg.mp3');
+const winSound = new Audio('./sound/game_win.mp3');
 
 
 btnPlay.addEventListener('click', () => {
-  if (!btnPlay.classList.contains('on')) {
+  if (!started) {
     gameStart();
   } else {
     gameOver();
   }
 })
-btnReplay.addEventListener('click', gameStart)
+btnReplay.addEventListener('click', gameStart);
 
 function gameStart() {
+  started = true;
   btnPlay.classList.add('on');
   gameResult.classList.remove('on');
-  gameArea.innerHTML = '';
   randomAmount = randomNumber(10) + 1;
   gameAmount.textContent = randomAmount;
   init(randomAmount);
   startTimer();
+  palySound(bgSound);
   gameArea.addEventListener('click', gamePlay);
 }
 function gameOver() {
+  started = false;
+  stopSound(bgSound);
   btnPlay.classList.remove('on');
   gameResult.classList.add('on');
   gameArea.removeEventListener('click', gamePlay);
   clearInterval(timer);
   if (randomAmount === 0) {
-    resultText.textContent = 'You Win🎉';
+    resultText.textContent = 'You Won🎉';
+    palySound(winSound);
   } else {
-    resultText.textContent = 'You Loose👎';
+    resultText.textContent = 'You Lost👎';
+    palySound(bugSound);
   }
 }
 function gamePlay(ev) {
@@ -48,37 +58,39 @@ function gamePlay(ev) {
   if (ev.target.className === 'bug') {
     gameOver();
   } else {
+    palySound(carrotSound);
     ev.target.remove();
     randomAmount--;
     gameAmount.textContent = randomAmount;
   }
   if (randomAmount === 0) gameOver();
 }
-function sound(src) {
-  this.sound = document.createElement('audio');
-  this.sound.src = src;
-  this.sound.setAttribute('preload', 'auto');
-  this.sound.setAttribute('controls', 'none');
-  this.sound.style.display = 'none';
-  gameArea.appendChild(this.sound);
-  this.play = () => this.sound.play()
-  this.stop = () => this.sound.pause();
+function palySound(sound) {
+  sound.currentTime = 0;
+  sound.play();
 }
-function updateTimer() {
-  if (timerLeft >= 0) {
-    gameTimer.textContent = `0:${timerLeft}`;
+function stopSound(sound) {
+  sound.pause();
+}
+function updateTimer(sec) {
+  const minutes = Math.floor(sec / 60);
+  const seconds = sec % 60;
+  if (sec >= 0) {
+    gameTimer.textContent = `${minutes}:${seconds}`;
   }
-  if (timerLeft === 0) {
+  if (sec <= 0) {
     gameOver();
   }
-  timerLeft -= 1;
 }
 function startTimer() {
-  timerLeft = 5;
-  timer = setInterval(updateTimer, 1000);
-  updateTimer();
+  let timerSec = timerLeft;
+  updateTimer(timerSec);
+  timer = setInterval(() => {
+    updateTimer(--timerSec);
+  }, 1000);
 }
 function init(num) {
+  gameArea.innerHTML = '';
   addItem('carrot', num);
   addItem('bug', num);
 }
